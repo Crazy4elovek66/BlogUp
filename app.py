2928571215b0dc85edf56524e56d8fa95cb7e20c
Crazy4elovek <80841013+Crazy4elovek66@users.noreply.github.com>
2025-05-11 22:25:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,55 @@
-from flask import Flask, request, jsonify, render_template
-from database import db, User
-import psycopg2
 import os
+from flask import Flask, request, jsonify, render_template
+from extensions import db
+from database import User
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/init_user', methods=['POST'])
-def init_user():
-    data = request.json
-    user_id = data.get('user_id')
+def create_app():
+    app = Flask(__name__)
     
-    user = User.query.filter_by(user_id=user_id).first()
-    if not user:
-        user = User(user_id=user_id)
-        db.session.add(user)
-        db.session.commit()
+    # Конфигурация БД
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    return jsonify({'views': user.views})
-
-@app.route('/add_view', methods=['POST'])
-
-# Подключение к PostgreSQL на Railway
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        port=os.getenv('DB_PORT')
-    )
-    return conn
-
-@app.route('/init')
-def init_user():
-    user_id = request.args.get('user_id')
-    conn = get_db_connection()
-    cur = conn.cursor()
+    # Инициализация расширений
+    db.init_app(app)
     
-    # Проверяем существование пользователя
-    cur.execute('SELECT views, level FROM users WHERE user_id = %s', (user_id,))
-    user = cur.fetchone()
-    
-    if not user:
-        # Создаем нового пользователя
-        cur.execute('INSERT INTO users (user_id) VALUES (%s) RETURNING views, level', (user_id,))
-        user = cur.fetchone()
-    
-    conn.commit()
-    cur.close()
-    conn.close()
-    
-    return jsonify({
-        'views': user[0],
-        'level': user[1]
-    })
-
-@app.route('/add_view', methods=['POST'])
-def add_view():
-    data = request.json
-    user_id = data.get('user_id')
-    
-    user = User.query.filter_by(user_id=user_id).first()
-    if user:
-        user.views += 1
-        db.session.commit()
-        return jsonify({'success': True, 'views': user.views})
-    return jsonify({'success': False})
-    
-    return jsonify({
-        'views': updated[0],
-        'level': updated[1]
-    })
-
-if __name__ == '__main__':
+    # Создание таблиц при первом запуске
     with app.app_context():
         db.create_all()
+    
+    # Маршруты
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/init_user', methods=['POST'])
+    def init_user():
+        data = request.json
+        user_id = data.get('user_id')
+        
+        user = User.query.filter_by(user_id=user_id).first()
+        if not user:
+            user = User(user_id=user_id)
+            db.session.add(user)
+            db.session.commit()
+        
+        return jsonify({'views': user.views})
+    
+    @app.route('/add_view', methods=['POST'])
+    def add_view():
+        data = request.json
+        user_id = data.get('user_id')
+        
+        user = User.query.filter_by(user_id=user_id).first()
+        if user:
+            user.views += 1
+            db.session.commit()
+            return jsonify({'success': True, 'views': user.views})
+        return jsonify({'success': False})
+    
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
     app.run(debug=True)
